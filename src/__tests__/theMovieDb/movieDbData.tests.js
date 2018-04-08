fe966e0b4d19853997e67d3620aa5116afb9fc59
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-04-08 14:01:10 +0100

    Added functions to transform data to movie, cast and videos

--- src/__tests__/theMovieDb/movieDbData.tests.js
+++ src/__tests__/theMovieDb/movieDbData.tests.js
@@ -1,6 +1,9 @@
 import MovieDbData from '../../theMovieDb/movieDbData';
 import configurationData from '../configurationData.json';
 import searchData from '../searchData.json';
+import movieData from '../movieData.json';
+import creditsData from '../creditsData.json';
+import videosData from '../videosData.json';
 
 describe('movieDbData', () => {
     describe('configuration', () => {
@@ -26,6 +29,59 @@ describe('movieDbData', () => {
 
         it('should create a url for movie posters', () => {
             expect(movies[0].posterImage).toBe(`${config.baseImageUrl}w300/hfjfjYsTBHgfogLWWiTm5OP7KpD.jpg`);
+        });
+    });
+
+    describe('movie', () => {
+        const config = { baseImageUrl: 'http://image.tmdb.org/t/p/' };
+        const movie = MovieDbData.movie(movieData, config);
+
+        it('should return the movie details', () => {
+            expect(movie.id).toBe(116149);
+            expect(movie.title).toBe('Paddington');
+            expect(movie.tagLine).toBe('THE ADVENTURE BEGINS');
+            expect(movie.overview).toBe('After a deadly earthquake destroys his home in Peruvian rainforest,' +
+                ' a young bear makes his way to England in search of a new home. The bear, dubbed "Paddington"' +
+                ' for the london train station, finds shelter with the family of Henry and Mary Brown.' +
+                ' Although Paddington\'s amazement at urban living soon endears him to the Browns, someone' +
+                ' else has her eye on him: Taxidermist Millicent Clyde has designs on the rare bear and his hide.');
+            expect(movie.genres).toEqual(['Comedy', 'Adventure']);
+        });
+
+        it('should create a url for the movie poster', () => {
+            expect(movie.posterImage).toBe(`${config.baseImageUrl}w300/hfjfjYsTBHgfogLWWiTm5OP7KpD.jpg`);
+        });
+    });
+
+    describe('cast', () => {
+        const config = { baseImageUrl: 'http://image.tmdb.org/t/p/' };
+        const cast = MovieDbData.cast(creditsData, config);
+
+        it('should return a list of cast member', () => {
+            expect(cast).toHaveLength(69);
+        });
+
+        it('should return details for each cast member', () => {
+            expect(cast[0].id).toBe(19);
+            expect(cast[0].character).toBe('Paddington (voice)');
+            expect(cast[0].name).toBe('Ben Whishaw');
+        });
+
+        it('should create a url for the cast member profile', () => {
+            expect(cast[0].profileImage).toBe(`${config.baseImageUrl}w185/e74Cz65ifeNb9zHfUTvF5rTix0d.jpg`);
+        });
+    });
+
+    describe('youtubeVideos', () => {
+        const videos = MovieDbData.youtubeVideos(videosData);
+        it('should return a list of youtube videos', () => {
+            expect(videos).toHaveLength(1);
+        });
+
+        it('should return details for each video', () => {
+            expect(videos[0].id).toBe('539f7416c3a36850df00003c');
+            expect(videos[0].name).toBe('PADDINGTON - Trailer 2 - On DVD, Blu-ray and Download now');
+            expect(videos[0].key).toBe('qFuzMlfZGWM');
         });
     });
 });
--- src/theMovieDb/movieDbData.js
+++ src/theMovieDb/movieDbData.js
@@ -10,12 +10,38 @@
  * @property posterImage
  */
 
+/** @typedef {Object} Movie
+ * @property id
+ * @property title
+ * @property tagLine
+ * @property overview
+ * @property genres a list of the genres attached to the movie
+ * @property posterImage
+ */
+
+/** @typedef {Object} CastMember
+ * @property id
+ * @property name
+ * @property character
+ * @property profileImage
+ */
+
+/** @typedef {Object} Video
+ * @property id
+ * @property name
+ * @property key
+ */
+
 const searchPosterSize = 'w300';
+const profileImageSize = 'w185';
+
+const YouTubeSite = 'YouTube';
 
 const createImageUrl = (baseUrl, size, imagePath) =>
     `${baseUrl}${size}${imagePath}`;
 
 const createPosterImageUrl = (baseUrl, imagePath) => createImageUrl(baseUrl, searchPosterSize, imagePath);
+const createProfileImageUrl = (baseUrl, imagePath) => createImageUrl(baseUrl, profileImageSize, imagePath);
 
 const MovieDbData = () => {
     /** @function configuration
@@ -38,9 +64,52 @@ const MovieDbData = () => {
             posterImage: createPosterImageUrl(config.baseImageUrl, movie.poster_path),
         }));
 
+    /** @function movie
+     * @param movieDbMovie the data received from the movie api
+     * @param {Configuration} config  the configuration
+     * @returns {Movie}
+     */
+    const movie = (movieDbMovie, config) => ({
+        id: movieDbMovie.id,
+        title: movieDbMovie.title,
+        tagLine: movieDbMovie.tagline,
+        overview: movieDbMovie.overview,
+        genres: movieDbMovie.genres.map(genre => genre.name),
+        posterImage: createPosterImageUrl(config.baseImageUrl, movieDbMovie.poster_path),
+    });
+
+    /** @function cast
+     * @param movieDbCredits the data received from the credits api
+     * @param {Configuration} config  the configuration
+     * @returns {CastMember[]}
+     */
+    const cast = (movieDbCredits, config) =>
+        movieDbCredits.cast.map(castMember => ({
+            id: castMember.cast_id,
+            character: castMember.character,
+            name: castMember.name,
+            profileImage: createProfileImageUrl(config.baseImageUrl, castMember.profile_path),
+        }));
+
+    /** @function youtubeVideos
+     * @param movieDbVideos the data received from the videos api
+     * @returns {Video[]}
+     */
+    const youtubeVideos = movieDbVideos =>
+        movieDbVideos.results
+            .filter(video => video.site === YouTubeSite)
+            .map(video => ({
+                id: video.id,
+                name: video.name,
+                key: video.key,
+            }));
+
     return {
         configuration,
         search,
+        movie,
+        cast,
+        youtubeVideos,
     };
 };
 
