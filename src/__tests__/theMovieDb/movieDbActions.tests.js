25d871ea90a43f069962ba23d8a9d1dea72e0eb6
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-04-08 14:12:21 +0100

    Added actions to get movie, cast and videos

--- src/__tests__/theMovieDb/movieDbActions.tests.js
+++ src/__tests__/theMovieDb/movieDbActions.tests.js
@@ -1,11 +1,17 @@
 import MovieDbActions from '../../theMovieDb/movieDbActions';
 import configurationData from '../configurationData.json';
 import searchData from '../searchData.json';
+import movieData from '../movieData.json';
+import creditsData from '../creditsData.json';
+import videosData from '../videosData.json';
 
 describe('movieDbActions', () => {
     const mockApi = ({
         getConfig: jest.fn(() => Promise.resolve(configurationData)),
         searchMovie: jest.fn(() => Promise.resolve(searchData)),
+        getMovie: jest.fn(() => Promise.resolve(movieData)),
+        getCredits: jest.fn(() => Promise.resolve(creditsData)),
+        getVideos: jest.fn(() => Promise.resolve(videosData)),
     });
 
     const mockConfig = ({
@@ -13,10 +19,16 @@ describe('movieDbActions', () => {
     });
 
     const mockSearch = [{ id: 1 }];
+    const mockMovie = { id: 1 };
+    const mockCast = { id: 2 };
+    const mockVideos = { id: 3 };
 
     const mockDataTransform = ({
         configuration: jest.fn(() => mockConfig),
         search: jest.fn(() => mockSearch),
+        movie: jest.fn(() => mockMovie),
+        cast: jest.fn(() => mockCast),
+        youtubeVideos: jest.fn(() => mockVideos),
     });
 
     const actions = MovieDbActions(mockApi, mockDataTransform);
@@ -51,6 +63,65 @@ describe('movieDbActions', () => {
             const searchText = 'Paddington';
             return actions.search(searchText, mockConfig).then((data) => {
                 expect(data).toEqual(mockSearch);
+            });
+        });
+    });
+
+    describe('getMovie', () => {
+        it('should call getMovie on the api and the data transformation', () => {
+            const movieId = 9999;
+            return actions.getMovie(movieId, mockConfig).then(() => {
+                expect(mockApi.getMovie.mock.calls).toHaveLength(1);
+                expect(mockApi.getMovie.mock.calls[0][0]).toEqual(movieId);
+                expect(mockDataTransform.movie.mock.calls).toHaveLength(1);
+                expect(mockDataTransform.movie.mock.calls[0][0]).toEqual(movieData);
+                expect(mockDataTransform.movie.mock.calls[0][1]).toEqual(mockConfig);
+            });
+        });
+
+        it('should return a promise with the transformation result', () => {
+            const movieId = 9999;
+            return actions.getMovie(movieId, mockConfig).then((data) => {
+                expect(data).toEqual(mockMovie);
+            });
+        });
+    });
+
+    describe('getCast', () => {
+        it('should call getCredits on the api and the data transformation', () => {
+            const movieId = 9999;
+            return actions.getCast(movieId, mockConfig).then(() => {
+                expect(mockApi.getCredits.mock.calls).toHaveLength(1);
+                expect(mockApi.getCredits.mock.calls[0][0]).toEqual(movieId);
+                expect(mockDataTransform.cast.mock.calls).toHaveLength(1);
+                expect(mockDataTransform.cast.mock.calls[0][0]).toEqual(creditsData);
+                expect(mockDataTransform.cast.mock.calls[0][1]).toEqual(mockConfig);
+            });
+        });
+
+        it('should return a promise with the transformation result', () => {
+            const movieId = 9999;
+            return actions.getCast(movieId, mockConfig).then((data) => {
+                expect(data).toEqual(mockCast);
+            });
+        });
+    });
+
+    describe('getYoutubeVideos', () => {
+        it('should call getVideos on the api and the data transformation', () => {
+            const movieId = 9999;
+            return actions.getYoutubeVideos(movieId).then(() => {
+                expect(mockApi.getVideos.mock.calls).toHaveLength(1);
+                expect(mockApi.getVideos.mock.calls[0][0]).toEqual(movieId);
+                expect(mockDataTransform.youtubeVideos.mock.calls).toHaveLength(1);
+                expect(mockDataTransform.youtubeVideos.mock.calls[0][0]).toEqual(videosData);
+            });
+        });
+
+        it('should return a promise with the transformation result', () => {
+            const movieId = 9999;
+            return actions.getYoutubeVideos(movieId).then((data) => {
+                expect(data).toEqual(mockVideos);
             });
         });
     });
--- src/theMovieDb/movieDbActions.js
+++ src/theMovieDb/movieDbActions.js
@@ -5,9 +5,21 @@ const MovieDbActions = (api, data) => {
     const search = (searchText, config) =>
         api.searchMovie(searchText).then(searchData => data.search(searchData, config));
 
+    const getMovie = (id, config) =>
+        api.getMovie(id).then(movieData => data.movie(movieData, config));
+
+    const getCast = (id, config) =>
+        api.getCredits(id).then(creditsData => data.cast(creditsData, config));
+
+    const getYoutubeVideos = id =>
+        api.getVideos(id).then(videosData => data.youtubeVideos(videosData));
+
     return {
         getConfig,
         search,
+        getMovie,
+        getCast,
+        getYoutubeVideos,
     };
 };
 
